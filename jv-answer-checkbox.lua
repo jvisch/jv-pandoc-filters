2ffcb81a13f086320e01ec3ad2a30b11357e5122
Jorg Visch <jorgvisch@gmail.com>
2022-10-26 21:47:48 +0200

    question answer checkbox

---
+++ jv-answer-checkbox.lua
@@ -0,0 +1,26 @@
+print("---- jv-answer-checkbox ------------------------------")
+-- if not FORMAT:match 'html' then
+--     print("!! jv-answer-checkbox can only be used on HTML output")
+-- end
+
+local counter = 0
+
+function addCheckBox(elm)
+    if FORMAT:match('html') then
+        if elm.classes:includes('answer') then
+            counter = counter + 1
+            local id = "answercheckbox" .. counter
+            return {
+                pandoc.RawInline("html", '<input type="checkbox" id="'..id..'" class="answer">'), 
+                pandoc.RawInline("html", '<label class="answer" for="'..id..'">antwoord</label>'), 
+                elm
+            }
+        end
+    end
+    return nil -- no changes
+end
+
+-- Expliciete volgorde 
+return {{
+    Div = addCheckBox
+}}
